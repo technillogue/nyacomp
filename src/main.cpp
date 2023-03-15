697210b0346d37ff992878337c677f60139348da
technillogue <technillogue@gmail.com>
2023-03-15 15:04:06 -0400

    format and add debug

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <pybind11/pybind11.h>
 #include "nvcomp/gdeflate.hpp"
-#include "nvcomp.hpp" 
+#include "nvcomp.hpp"
 #include "nvcomp/nvcompManagerFactory.hpp"
 
 #include <chrono>
@@ -16,14 +16,15 @@
 using namespace nvcomp;
 namespace py = pybind11;
 
-
-#define CUDA_CHECK(cond)                                                       \
-  do {                                                                         \
-    cudaError_t err = cond;                                                    \
-    if (err != cudaSuccess) {                                               \
-      std::cerr << "Failure" << std::endl;                                \
-      exit(1);                                                              \
-    }                                                                         \
+#define CUDA_CHECK(cond)                   \
+  do                                       \
+  {                                        \
+    cudaError_t err = cond;                \
+    if (err != cudaSuccess)                \
+    {                                      \
+      std::cerr << "Failure" << std::endl; \
+      exit(1);                             \
+    }                                      \
   } while (false)
 
 /*
@@ -36,37 +37,41 @@ namespace py = pybind11;
   } while (false)
 */
 
-std::pair<std::vector<uint8_t>, size_t> load_file(const std::string& filename) {
+// read DEBUG flag from environment as a bool
+const bool DEBUG = std::getenv("DEBUG") ? std::stoi(std::getenv("DEBUG")) : false;
+
+void log(const std::string &msg)
+{
+  if (DEBUG)
+    std::cout << msg << std::endl;
+}
+
+std::pair<std::vector<uint8_t>, size_t> load_file(const std::string &filename)
+{
   std::ifstream file(filename, std::ios::binary | std::ios::ate);
-  if (!file.is_open()) {
+  if (!file.is_open())
     throw std::runtime_error("Failed to open file: " + filename);
-  }
 
   size_t file_size = static_cast<size_t>(file.tellg());
   std::vector<uint8_t> buffer(file_size);
 
   file.seekg(0, std::ios::beg);
-  if (!file.read(reinterpret_cast<char*>(buffer.data()), file_size)) {
+  if (!file.read(reinterpret_cast<char *>(buffer.data()), file_size))
+  {
     throw std::runtime_error("Failed to read file: " + filename);
   }
 
   return std::make_pair(buffer, file_size);
 }
 
-
-// float compress(const std::string input_filename) {
-//   std::vector<uint8_t> uncompressed_data;
-//   size_t input_buffer_len;
-//   std::tie(uncompressed_data, input_buffer_len) = load_file(filename);
-//   std::cout << "read " << input_buffer_len << " bytes from " << filename << std::endl;
-
-int compress(py::bytes pybytes, const std::string filename) {
+int compress(py::bytes pybytes, const std::string filename)
+{
   std::string bytes_str = pybytes;
   size_t input_buffer_len = bytes_str.size();
-  std::vector<uint8_t> uncompressed_data(bytes_str.data(), bytes_str.data() + input_buffer_len );
+  std::vector<uint8_t> uncompressed_data(bytes_str.data(), bytes_str.data() + input_buffer_len);
   std::cout << "working with " << input_buffer_len << " uncompressed bytes" << std::endl;
 
-  uint8_t* device_input_ptrs;
+  uint8_t *device_input_ptrs;
   CUDA_CHECK(cudaMalloc(&device_input_ptrs, input_buffer_len));
   CUDA_CHECK(cudaMemcpy(device_input_ptrs, uncompressed_data.data(), input_buffer_len, cudaMemcpyDefault));
 
@@ -80,13 +85,13 @@ int compress(py::bytes pybytes, const std::string filename) {
 
   GdeflateManager nvcomp_manager{chunk_size, data_type, stream};
   CompressionConfig comp_config = nvcomp_manager.configure_compression(input_buffer_len);
-  uint8_t* comp_buffer;
+  uint8_t *comp_buffer;
   CUDA_CHECK(cudaMalloc(&comp_buffer, comp_config.max_compressed_buffer_size));
 
   // check how long compression takes
 
   std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
- 
+
   nvcomp_manager.compress(device_input_ptrs, comp_buffer, comp_config);
   CUDA_CHECK(cudaStreamSynchronize(stream));
 
@@ -99,9 +104,10 @@ int compress(py::bytes pybytes, const std::string filename) {
   float comp_ratio = (float)comp_size / (float)input_buffer_len;
 
   // copy compressed buffer to host memory and then write it to a file
-  
+
   std::ofstream comp_file(filename, std::ios::binary);
-  if (!comp_file.is_open()) {
+  if (!comp_file.is_open())
+  {
     throw std::runtime_error("Failed to open file: " + filename);
   }
 
@@ -110,9 +116,8 @@ int compress(py::bytes pybytes, const std::string filename) {
   // copy compressed buffer to host memory using stream and syncronize to block for the copy to finish
   CUDA_CHECK(cudaMemcpy(comp_buffer_host.data(), comp_buffer, comp_size, cudaMemcpyDefault));
   std::cout << "writing compressed buffer to file: " << filename << std::endl;
-  comp_file.write(reinterpret_cast<const char*>(comp_buffer_host.data()), comp_size);
+  comp_file.write(reinterpret_cast<const char *>(comp_buffer_host.data()), comp_size);
   comp_file.close();
-  
 
   CUDA_CHECK(cudaFree(comp_buffer));
   CUDA_CHECK(cudaFree(device_input_ptrs));
@@ -123,31 +128,32 @@ int compress(py::bytes pybytes, const std::string filename) {
   return comp_size;
 }
 
-torch::ScalarType type_for_name(std::string type_name) {
-  if (type_name == "uint8") return torch::kUInt8;
-   else if (type_name == "int8") return torch::kInt8;
-   else if (type_name == "int16") return torch::kInt16;
-   else if (type_name == "int32") return torch::kInt32;
-   else if (type_name == "int64") return torch::kInt64;
-   else if (type_name == "float32") return torch::kFloat32;
-   else if (type_name == "float64") return torch::kFloat64;
-   else throw std::runtime_error("Unknown type name: " + type_name);
-}
+// torch::ScalarType type_for_name(std::string type_name) {
+//   if (type_name == "uint8") return torch::kUInt8;
+//    else if (type_name == "int8") return torch::kInt8;
+//    else if (type_name == "int16") return torch::kInt16;
+//    else if (type_name == "int32") return torch::kInt32;
+//    else if (type_name == "int64") return torch::kInt64;
+//    else if (type_name == "float32") return torch::kFloat32;
+//    else if (type_name == "float64") return torch::kFloat64;
+//    else throw std::runtime_error("Unknown type name: " + type_name);
+// }
 
 // maybe just decompress from an arbitrary stream + length?
 // doing an r2 call with a library probably sucks
 // can you just get a fd?
 // or a python iterator...?
 // maybe you can go into python to grab an http library
-// 
+//
 
-torch::Tensor decompress(const std::string filename, torch::Tensor tensor) {
+torch::Tensor decompress(const std::string filename, torch::Tensor tensor)
+{
   std::vector<uint8_t> compressed_data;
   size_t input_buffer_len;
   std::tie(compressed_data, input_buffer_len) = load_file(filename);
   std::cout << "read " << input_buffer_len << " bytes from " << filename << std::endl;
   std::chrono::steady_clock::time_point copy_begin = std::chrono::steady_clock::now();
-  uint8_t* comp_buffer;
+  uint8_t *comp_buffer;
   // while file is not eof or length not reached
   // read file into buffer
   // cudaMemcpy 128kb ?
@@ -177,12 +183,12 @@ torch::Tensor decompress(const std::string filename, torch::Tensor tensor) {
   // torch::Tensor decomp_data = torch::empty({decomp_config.decomp_data_size / element_size}, tensor.options());
 
   // // torch::Tensor decomp_data = torch::empty(tensor.sizes(), torch::TensorOptions().dtype(torch::kUInt8).device(tensor.device()));
-  decomp_nvcomp_manager->decompress(static_cast<uint8_t*>(tensor.data_ptr()), comp_buffer, decomp_config);
+  decomp_nvcomp_manager->decompress(static_cast<uint8_t *>(tensor.data_ptr()), comp_buffer, decomp_config);
   // // decomp_nvcomp_manager->decompress(decomp_data.data_ptr<uint8_t>(), comp_buffer, decomp_config);
   // decomp_nvcomp_manager->decompress(decomp_buffer, comp_buffer, decomp_config);
   CUDA_CHECK(cudaStreamSynchronize(stream));
-  std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now(); 
-  std::cout << "decompression time: " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << "[µs]" << std::endl;  // std::cout << "synced" << std::endl;
+  std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+  std::cout << "decompression time: " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << "[µs]" << std::endl; // std::cout << "synced" << std::endl;
   // create a new tensor using from_blob by passing the same data_ptr, to reinterpret the same data as the correct dtype
   // torch::Tensor decomp_tensor = torch::from_blob(decomp_buffer, tensor.sizes(), tensor.options());
   // std::cout << "did from_blob" << std::endl;
@@ -191,7 +197,7 @@ torch::Tensor decompress(const std::string filename, torch::Tensor tensor) {
   // std::cout << "decomp_tensor sizes: " << decomp_tensor.sizes() << std::endl;
   // std::cout << "tensor data_ptr: " << tensor.data_ptr() << std::endl;
   // std::cout << "decomp_tensor data_ptr: " << decomp_tensor.data_ptr() << std::endl;
-  
+
   // tensor.copy_(decomp_tensor);
   // tensor.set_(decomp_data);
   // Cast the decompressed data to the correct type before copying it to the output tensor
@@ -206,14 +212,6 @@ torch::Tensor decompress(const std::string filename, torch::Tensor tensor) {
   return tensor;
 }
 
-// torch::Tensor& make_tensor() {
-//     // auto options = torch::TensorOptions().dtype(torch::kFloat32).device(torch::kCUDA);
-//     // auto tensor = torch::empty({1024 / sizeof(float)}, options);
-//     torch::Tensor tensor = torch::ones(5);
-//     // std::cout << "made tensor" << std::endl;
-//     return torch::ones(5);;
-// }
-
 // std::shared_ptr<torch::Tensor> make_tensor() {
 //     auto tensor = std::make_shared<torch::Tensor>(torch::ones(5));
 //     std::cout << "made tensor" << std::endl;
@@ -221,48 +219,43 @@ torch::Tensor decompress(const std::string filename, torch::Tensor tensor) {
 // }
 
 // torch::Tensor make_tensor() {
-//     auto tensor = torch::ones(5);
-//     return tensor;
+//   auto tensor_options = torch::TensorOptions().dtype(torch::kUInt8).device(torch::kCUDA, 0);
+//   torch::Tensor tensor = torch::empty({100}, tensor_options);
+//   return tensor;
 // }
 
-torch::Tensor make_tensor() {
-  auto tensor_options = torch::TensorOptions().dtype(torch::kUInt8).device(torch::kCUDA, 0);
-  torch::Tensor tensor = torch::empty({100}, tensor_options);
-  return tensor;
-}
+// torch::Tensor d_sigmoid(torch::Tensor z) {
+//   auto s = torch::sigmoid(z);
+//   return (1 - s) * s;
+// }
 
-torch::Tensor d_sigmoid(torch::Tensor z) {
-  auto s = torch::sigmoid(z);
-  return (1 - s) * s;
-}
+PYBIND11_MODULE(_nyacomp, m)
+{
+  m.doc() = R"pbdoc(python bindings for nvcomp with torch)pbdoc";
 
-
-PYBIND11_MODULE(_nyacomp, m) {
-    m.doc() = R"pbdoc(python bindings for nvcomp with torch)pbdoc";
-
-    m.def("compress", &compress, R"pbdoc(
+  m.def("compress", &compress, R"pbdoc(
         compress
-    )pbdoc",  py::arg("data"), py::arg("filename"));
+    )pbdoc",
+        py::arg("data"), py::arg("filename"));
 
-    m.def("decompress", &decompress, R"pbdoc(
+  m.def("decompress", &decompress, R"pbdoc(
         decompress
-    )pbdoc",  py::arg("filename"), py::arg("dest_tensor"));
+    )pbdoc",
+        py::arg("filename"), py::arg("dest_tensor"));
 
-    // m.def("sigmoid", &d_sigmoid, "sigmod fn");
+  // m.def("sigmoid", &d_sigmoid, "sigmod fn");
 
-    // m.def("make_tensor", &make_tensor, py::return_value_policy::automatic);
+  // m.def("make_tensor", &make_tensor, py::return_value_policy::automatic);
 
-    // m.def("make_tensor", &make_tensor, "make_tensor");
+  // m.def("subtract", [](int i, int j) { return i - j; }, R"pbdoc(
+  //     Subtract two numbers
 
-    // m.def("subtract", [](int i, int j) { return i - j; }, R"pbdoc(
-    //     Subtract two numbers
-
-    //     Some other explanation about the subtract function.
-    // )pbdoc");
+  //     Some other explanation about the subtract function.
+  // )pbdoc");
 
 #ifdef VERSION_INFO
-    m.attr("__version__") = MACRO_STRINGIFY(VERSION_INFO);
+  m.attr("__version__") = MACRO_STRINGIFY(VERSION_INFO);
 #else
-    m.attr("__version__") = "dev";
+  m.attr("__version__") = "dev";
 #endif
 }
