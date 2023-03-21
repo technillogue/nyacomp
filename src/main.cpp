7fd3b5742361cce3f1dc1c845bf7509818ecd691
technillogue <technillogue@gmail.com>
2023-03-21 17:09:15 -0400

    reuse managers

--- src/main.cpp
+++ src/main.cpp
@@ -540,7 +540,6 @@ void compress_lowlevel(char* input_data, const size_t in_bytes, const std::strin
 }
 
 
-
 // void decompress_lowlevel(const std::string& filename, const size_t decompressed_size, const size_t batch_size)
 // {
 //   // Read compressed data from file
@@ -553,9 +552,7 @@ void compress_lowlevel(char* input_data, const size_t in_bytes, const std::strin
 //   cudaStream_t stream;
 //   cudaStreamCreate(&stream);
 
-
 //   const size_t chunk_size = 65536;
-
 
 //   size_t* device_uncompressed_bytes;
 //   void ** device_uncompressed_ptrs;
@@ -565,9 +562,7 @@ void compress_lowlevel(char* input_data, const size_t in_bytes, const std::strin
 //   void** device_compressed_ptrs;
 //   cudaMalloc(&device_compressed_ptrs, sizeof(size_t) * batch_size);
 
-  
-//   static const nvcompBatchedGdeflateOpts_t opts = {0};
-
+//   static const nvcompBatchedGdeflateOpts_t opts = {1};
 
 //   // ... (Initialize the necessary variables for decompression)
 
@@ -583,9 +578,6 @@ void compress_lowlevel(char* input_data, const size_t in_bytes, const std::strin
 //   in_file.close();
 
 
-
-
-// ////
 //   // Decompression can be similarly performed on a batch of multiple compressed input chunks. 
 //   // As no metadata is stored with the compressed data, chunks can be re-arranged as well as decompressed 
 //   // with other chunks that originally were not compressed in the same batch.
@@ -772,7 +764,7 @@ std::vector<torch::Tensor> batch_decompress_threadpool(
       auto create_stream_begin = std::chrono::steady_clock::now();
       for (int stream_id = 0; stream_id < streams_per_thread; stream_id++) 
         CUDA_CHECK(cudaStreamCreate(&streams[thread_id][stream_id]));
-      
+      std::vector<std::shared_ptr<nvcomp::nvcompManagerBase>> managers(streams_per_thread); 
 
       log(std::to_string(thread_id) + ": creating streams took " + std::to_string(std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now() - create_stream_begin).count()) + "[µs]");
       // cudaStream_t stream = streams[thread_id];
@@ -812,8 +804,17 @@ std::vector<torch::Tensor> batch_decompress_threadpool(
 
         tensors[i] = make_tensor(tensor_shapes[i], dtypes[i]);
         debug(prefix + "creating manager with stream " + std::to_string(stream_int));
+        std::shared_ptr<nvcomp::nvcompManagerBase> decomp_nvcomp_manager = managers[job_number % streams_per_thread];
+        // check if manager was already created
+        if (!decomp_nvcomp_manager) {
+          log("creating manager for stream " + std::to_string(stream_int) + " (job " + std::to_string(job_number) + ")");
+          decomp_nvcomp_manager = create_manager(comp_buffer, stream);
+          managers[job_number % streams_per_thread] = decomp_nvcomp_manager;
+        } else {
+          log("reusing manager for stream " + std::to_string(stream_int) + " (job " + std::to_string(job_number) + ")");
+        }
         // this syncs the stream
-        auto decomp_nvcomp_manager = create_manager(comp_buffer, stream);
+        // std::shared_ptr<nvcomp::nvcompManagerBase> decomp_nvcomp_manager = create_manager(comp_buffer, stream);
         debug(prefix + "configuring decomp");
         DecompressionConfig decomp_config = decomp_nvcomp_manager->configure_decompression(comp_buffer);
         auto decomp_begin = std::chrono::steady_clock::now();
@@ -882,6 +883,149 @@ std::vector<torch::Tensor> batch_decompress_threadpool(
   log("Total copy time: " + std::to_string(total_copy_time) + "[µs], total decomp time: " + std::to_string(total_decomp_time) + "[µs]");
 
   return tensors;
+}
+
+
+
+void lowlevel_example(char* input_data, const size_t in_bytes)
+{
+  cudaStream_t stream;
+  cudaStreamCreate(&stream);
+
+  // First, initialize the data on the host.
+
+  // compute chunk sizes
+  size_t* host_uncompressed_bytes;
+  const size_t chunk_size = 65536;
+  const size_t batch_size = (in_bytes + chunk_size - 1) / chunk_size;
+
+  char* device_input_data;
+  cudaMalloc(&device_input_data, in_bytes);
+  cudaMemcpyAsync(device_input_data, input_data, in_bytes, cudaMemcpyHostToDevice, stream);
+
+  cudaMallocHost(&host_uncompressed_bytes, sizeof(size_t)*batch_size);
+  for (size_t i = 0; i < batch_size; ++i) {
+    if (i + 1 < batch_size) {
+      host_uncompressed_bytes[i] = chunk_size;
+    } else {
+      // last chunk may be smaller
+      host_uncompressed_bytes[i] = in_bytes - (chunk_size*i);
+    }
+  }
+
+  // Setup an array of pointers to the start of each chunk
+  void ** host_uncompressed_ptrs;
+  cudaMallocHost(&host_uncompressed_ptrs, sizeof(size_t)*batch_size);
+  for (size_t ix_chunk = 0; ix_chunk < batch_size; ++ix_chunk) {
+    host_uncompressed_ptrs[ix_chunk] = device_input_data + chunk_size*ix_chunk;
+  }
+
+  size_t* device_uncompressed_bytes;
+  void ** device_uncompressed_ptrs;
+  cudaMalloc(&device_uncompressed_bytes, sizeof(size_t) * batch_size);
+  cudaMalloc(&device_uncompressed_ptrs, sizeof(size_t) * batch_size);
+  
+  cudaMemcpyAsync(device_uncompressed_bytes, host_uncompressed_bytes, sizeof(size_t) * batch_size, cudaMemcpyHostToDevice, stream);
+  cudaMemcpyAsync(device_uncompressed_ptrs, host_uncompressed_ptrs, sizeof(size_t) * batch_size, cudaMemcpyHostToDevice, stream);
+
+  // Then we need to allocate the temporary workspace and output space needed by the compressor.
+  size_t temp_bytes;
+  nvcompBatchedLZ4CompressGetTempSize(batch_size, chunk_size, nvcompBatchedLZ4DefaultOpts, &temp_bytes);
+  void* device_temp_ptr;
+  cudaMalloc(&device_temp_ptr, temp_bytes);
+
+  // get the maxmimum output size for each chunk
+  size_t max_out_bytes;
+  nvcompBatchedLZ4CompressGetMaxOutputChunkSize(chunk_size, nvcompBatchedLZ4DefaultOpts, &max_out_bytes);
+
+  // Next, allocate output space on the device
+  void ** host_compressed_ptrs;
+  cudaMallocHost(&host_compressed_ptrs, sizeof(size_t) * batch_size);
+  for(size_t ix_chunk = 0; ix_chunk < batch_size; ++ix_chunk) {
+      cudaMalloc(&host_compressed_ptrs[ix_chunk], max_out_bytes);
+  }
+
+  void** device_compressed_ptrs;
+  cudaMalloc(&device_compressed_ptrs, sizeof(size_t) * batch_size);
+  cudaMemcpyAsync(
+      device_compressed_ptrs, host_compressed_ptrs, 
+      sizeof(size_t) * batch_size,cudaMemcpyHostToDevice, stream);
+
+  // allocate space for compressed chunk sizes to be written to
+  size_t * device_compressed_bytes;
+  cudaMalloc(&device_compressed_bytes, sizeof(size_t) * batch_size);
+
+  // And finally, call the API to compress the data
+  nvcompStatus_t comp_res = nvcompBatchedLZ4CompressAsync(
+      device_uncompressed_ptrs,
+      device_uncompressed_bytes,
+      chunk_size, // The maximum chunk size
+      batch_size,
+      device_temp_ptr,
+      temp_bytes,
+      device_compressed_ptrs,
+      device_compressed_bytes,
+      nvcompBatchedLZ4DefaultOpts,
+      stream);
+
+  if (comp_res != nvcompSuccess)
+  {
+    std::cerr << "Failed compression!" << std::endl;
+    assert(comp_res == nvcompSuccess);
+  }
+
+  // Decompression can be similarly performed on a batch of multiple compressed input chunks. 
+  // As no metadata is stored with the compressed data, chunks can be re-arranged as well as decompressed 
+  // with other chunks that originally were not compressed in the same batch.
+
+  // If we didn't have the uncompressed sizes, we'd need to compute this information here. 
+  // We demonstrate how to do this.
+  nvcompBatchedLZ4GetDecompressSizeAsync(
+      device_compressed_ptrs,
+      device_compressed_bytes,
+      device_uncompressed_bytes,
+      batch_size,
+      stream);
+
+  // Next, allocate the temporary buffer 
+  size_t decomp_temp_bytes;
+  nvcompBatchedLZ4DecompressGetTempSize(batch_size, chunk_size, &decomp_temp_bytes);
+  void * device_decomp_temp;
+  cudaMalloc(&device_decomp_temp, decomp_temp_bytes);
+
+  // allocate statuses
+  nvcompStatus_t* device_statuses;
+  cudaMalloc(&device_statuses, sizeof(nvcompStatus_t)*batch_size);
+
+  // Also allocate an array to store the actual_uncompressed_bytes.
+  // Note that we could use nullptr for this. We already have the 
+  // actual sizes computed during the call to nvcompBatchedLZ4GetDecompressSizeAsync.
+  size_t* device_actual_uncompressed_bytes;
+  cudaMalloc(&device_actual_uncompressed_bytes, sizeof(size_t)*batch_size);
+
+  // And finally, call the decompression routine.
+  // This decompresses each input, device_compressed_ptrs[i], and places the decompressed
+  // result in the corresponding output list, device_uncompressed_ptrs[i]. It also writes
+  // the size of the uncompressed data to device_uncompressed_bytes[i].
+  nvcompStatus_t decomp_res = nvcompBatchedLZ4DecompressAsync(
+      device_compressed_ptrs, 
+      device_compressed_bytes, 
+      device_uncompressed_bytes, 
+      device_actual_uncompressed_bytes, 
+      batch_size,
+      device_decomp_temp, 
+      decomp_temp_bytes, 
+      device_uncompressed_ptrs, 
+      device_statuses, 
+      stream);
+  
+  if (decomp_res != nvcompSuccess)
+  {
+    std::cerr << "Failed compression!" << std::endl;
+    assert(decomp_res == nvcompSuccess);
+  }
+
+  cudaStreamSynchronize(stream);
 }
 
 
